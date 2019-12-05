68fd1f5a27cc58233f71dedbc48b94bc603fe168
Ross Kinsey <RKinsey@gmu.edu>
2019-12-04 19:56:00 -0500

    sd

---
+++ my-blog/gatsby-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: "My Blog",
+    author: "Ross Kinsey",
+    description: "Learning Gatsby themes",
+    siteUrl: "https://amberley.blog/",
+    social: [
+      {
+        name: "twitter",
+        url: "https://twitter.com/amber1ey",
+      },
+      {
+        name: "github",
+        url: "https://github.com/amberleyromo",
+      },
+    ],
+  },
+}
